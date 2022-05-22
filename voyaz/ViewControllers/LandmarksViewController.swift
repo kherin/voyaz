9bb9112468cbfb83e4dd82f931ba688c037e3f7f
kherin bundhoo <kherin.j.s.bundhoo@gmail.com>
2022-05-22 17:18:43 +0400

    feat(filtering): storing  and filtering favorite landmarks

--- voyaz/ViewControllers/LandmarksViewController.swift
+++ voyaz/ViewControllers/LandmarksViewController.swift
@@ -10,12 +10,15 @@ import CoreData
 
 class LandmarksViewController: UITableViewController {
     var isFavoriteFilterActive: Bool = false
-    var filteredLandmarks: [Landmark] = []
     var landmarkDataSource = LandmarksDataSource()
-    var loadedLandmarks: [Landmark] = []
     
     @IBAction func onFilterFavoritesSwitchValueChanged(_ sender: UISwitch) {
         isFavoriteFilterActive = sender.isOn
+        guard let appDelegate =
+                UIApplication.shared.delegate as? AppDelegate else {
+            return
+        }
+        let _ = LandmarksDataSource.fetch(appDelegate: appDelegate)
         self.tableView.reloadData()
     }
     
@@ -29,20 +32,26 @@ class LandmarksViewController: UITableViewController {
         }
         
         // MARK - Preloading landmarks data
-        LandmarksDataSource.preload(appDelegate: appDelegate)
+//        LandmarksDataSource.preload(appDelegate: appDelegate)
+        let _ = LandmarksDataSource.fetch(appDelegate: appDelegate)
     }
 }
 
 extension LandmarksViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return LandmarksDataSource.getCount(onlyFavorites: isFavoriteFilterActive)
+        guard let appDelegate =
+                UIApplication.shared.delegate as? AppDelegate else {
+            return 0
+        }
+        return LandmarksDataSource.getCount(appDelegate: appDelegate, onlyFavorites: isFavoriteFilterActive)
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
         let cell = tableView.dequeueReusableCell(withIdentifier: "LandmarkCell", for: indexPath) as! LandmarkCell
         
         cell.landmark = LandmarksDataSource.landmark(
+            appDelegate: appDelegate,
             at: indexPath,
             onlyFavorites: isFavoriteFilterActive
         )
@@ -54,8 +63,13 @@ extension LandmarksViewController {
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
         if let destination = segue.destination as? LandmarkDetailViewController {
-            destination.landmark = LandmarksDataSource.landmark(at: self.tableView.indexPathForSelectedRow!, onlyFavorites: isFavoriteFilterActive)
+            destination.landmark = LandmarksDataSource.landmark(
+                appDelegate: appDelegate,
+                at: self.tableView.indexPathForSelectedRow!,
+                onlyFavorites: isFavoriteFilterActive
+            )
         }
     }
 }
